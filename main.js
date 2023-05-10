37239c91e9784085efb12d94ce4571e238cf4f3f
lipeee <felipemendesofficial@gmail.com>
2023-05-10 19:56:38 -0300

    Troca de car por emoji

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const balls = document.querySelectorAll(".ball");
 const bigBall = document.querySelector(".bigball");
 
-const colors = ["black", "red", "blue", "purple", "green"];
+const colors = [
+  "&#128577;",
+  "&#128533;",
+  "&#128578;",
+  "&#128512;",
+  "&#128525;",
+];
 
 balls.forEach((ball, index) => {
   ball.addEventListener("click", (e) => {
     balls.forEach((ball, i) => {
-      ball.style.background = i <= index ? "yellow" : "";
-      bigBall.style.background = colors[index];
+      ball.style.color = i <= index ? "yellow" : "";
+      bigBall.style.backgroud = colors[index];
     });
   });
 });
