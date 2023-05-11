511933b07973fafba0b00111e8c6ddb9ac9ea74d
lipeee <felipemendesofficial@gmail.com>
2023-05-10 21:38:47 -0300

    Nova estilização

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const balls = document.querySelectorAll(".ball");
 const bigBall = document.querySelector(".bigball");
 
 const colors = [
-  "&#128577;",
+  "&#128543;",
   "&#128533;",
   "&#128578;",
   "&#128512;",
@@ -13,7 +13,7 @@ balls.forEach((ball, index) => {
   ball.addEventListener("click", (e) => {
     balls.forEach((ball, i) => {
       ball.style.color = i <= index ? "yellow" : "";
-      bigBall.style.backgroud = colors[index];
+      bigBall.innerHTML = colors[index];
     });
   });
 });
